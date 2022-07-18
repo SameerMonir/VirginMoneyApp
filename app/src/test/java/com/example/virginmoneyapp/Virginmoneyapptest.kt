ce5e0f4ad1e0454661eafc6fa87db4dd1cb60de5
sameermonir <sameer.monir23@gmail.com>
2022-07-18 10:56:36 +0100

    secCommit

--- app/src/test/java/com/example/virginmoneyapp/Virginmoneyapptest.kt
+++ app/src/test/java/com/example/virginmoneyapp/Virginmoneyapptest.kt
@@ -1,4 +1,50 @@
 package com.example.virginmoneyapp
 
+import com.example.virginmoneyapp.Api.ApiService
+import com.google.gson.Gson
+import junit.framework.Assert
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.MockitoAnnotations
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class Virginmoneyapptest {
+
+    lateinit var mockWebServer: MockWebServer
+    lateinit var apiService: ApiService
+    lateinit var gson: Gson
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        gson = Gson()
+        mockWebServer = MockWebServer()
+        apiService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build().create(ApiService::class.java)
+    }
+
+
+    @Test
+    fun `get all movie api test`() {
+        runBlocking {
+            val mockResponse = MockResponse()
+            mockWebServer.enqueue(mockResponse.setBody("[]"))
+            val response = apiService.getPeople()
+            val request = mockWebServer.takeRequest()
+            Assert.assertEquals("/people",request.path)
+            Assert.assertEquals(true, response.body()?.isEmpty() == true)
+        }
+    }
+
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
 }
